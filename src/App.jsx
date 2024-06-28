e961581d265733b272da46d6ee895f59b5f956d1
Patriciagracia <patriciagraciaherrero@gmail.com>
2024-06-28 15:33:09 +0200

    Feat: Speed Insights

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import { Outlet } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import NavBar from "./NavBar.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <NavBar />
       <Outlet />
+      <SpeedInsights />
     </I18nextProvider>
   );
 }
